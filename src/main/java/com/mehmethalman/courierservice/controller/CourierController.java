0fdd7189355d5f73ad864cf04e7fc8c8b61c82be
mehmethalman1940 <mehmethalman1940@gmail.com>
2026-07-14 16:03:42 +0300

    feat: implement shipment endpoints-Courier service

--- src/main/java/com/mehmethalman/courierservice/controller/CourierController.java
+++ src/main/java/com/mehmethalman/courierservice/controller/CourierController.java
@@ -1,13 +1,13 @@
 package com.mehmethalman.courierservice.controller;
 
+import com.mehmethalman.courierservice.dto.CourierAssignRequestDto;
 import com.mehmethalman.courierservice.dto.CourierDto;
 import com.mehmethalman.courierservice.service.CourierService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +21,16 @@ public class CourierController {
     public ResponseEntity<List<CourierDto>> getAvailableCouriers() {
         List<CourierDto> availableCouriers = courierService.getIsAvaibleCourier();
         return ResponseEntity.ok(availableCouriers);
+    }
+
+    @GetMapping("/get")
+    public List<CourierDto> getAllCouriers(){
+        return courierService.getAllCourier();
+    }
+
+    @PostMapping(path = "/{id}/assign")
+    public ResponseEntity<String> assignCourier(@PathVariable Long id, @RequestBody CourierAssignRequestDto courierAssignRequestDto) {
+        courierService.assignCourier(id, courierAssignRequestDto);
+        return ResponseEntity.ok("Kurye başarıyla kargoya atandı ve false durumuna getirildi.");
     }
 }
