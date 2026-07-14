e59998a47b8579afc7a235a99a51fa461d4dca03
mehmethalman1940 <mehmethalman1940@gmail.com>
2026-07-14 16:09:53 +0300

    feat: courier-service kurye atama kodları ve şema güncellemeleri eklendi

---
+++ courier-service/src/main/java/com/mehmethalman/courierservice/dto/CourierAssignRequestDto.java
@@ -0,0 +1,12 @@
+package com.mehmethalman.courierservice.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CourierAssignRequestDto {
+    private String shipmentTrackingNumber;
+
+}
