e88a9af74075bd25a6566bd59f69aee801e80657
mehmethalman1940 <mehmethalman1940@gmail.com>
2026-07-16 00:19:33 +0300

    feat: notification-service and retry

--- shipment-service/src/main/java/com/mehmethalman/shipmentservice/ShipmentServiceApplication.java
+++ shipment-service/src/main/java/com/mehmethalman/shipmentservice/ShipmentServiceApplication.java
@@ -3,7 +3,9 @@ package com.mehmethalman.shipmentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableRetry
 @EnableFeignClients
 @SpringBootApplication
 public class ShipmentServiceApplication {
